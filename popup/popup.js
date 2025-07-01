dce65716a8e798c716ced19db7d28a0144b57970
sonjali4 <sonjali2006@gmail.com>
2025-07-01 21:54:19 +1200

    updated start and pause features

--- popup/popup.js
+++ popup/popup.js
@@ -1,31 +1,60 @@
 let timesArray = [25, 5, 15];
 let currentIndex = 0;
 
-function startTimer() {
-    const minutes = timesArray[currentIndex];
-    chrome.action.setBadgeText({ text: 'ON' });
-    chrome.alarms.create('timer', { delayInMinutes: minutes });  // NOTE: 'timer' is the name of the alarm
-    chrome.storage.sync.set({ minutes: minutes });
+function startPauseTimer() {
+    chrome.alarms.get('timer', function(alarm) {
+        if (alarm) {
+            pauseTimer();
+        } else {
+            startTimer();
+        }
+    });
 }
 
-function resetTimer() {
-    chrome.action.setBadgeText({ text: '' });
-    chrome.alarms.clearAll();
-    document.getElementById("time").innerText = timesArray[currentIndex] + ":00";
+function startTimer() {
+    chrome.storage.local.get(['remainingSeconds'], function(result) {  // check for existing timer
+        const totalSeconds = result.remainingSeconds;
+        let minutes = 0;
+
+        if (totalSeconds && totalSeconds > 0) {  // use existing time remaining
+            minutes = totalSeconds / 60;
+        } else {  // use default time
+            minutes = timesArray[currentIndex];
+        }
+
+        chrome.action.setBadgeText({ text: 'ON' });
+        chrome.alarms.create('timer', { delayInMinutes: minutes });  // NOTE: 'timer' is the name of the alarm, maybe change name later ?
+        chrome.storage.local.set({ minutes: minutes });
+
+        document.getElementById("start-btn").innerText = "Pause";
+        chrome.storage.local.remove('remainingSeconds');
+    });
 }
 
 async function pauseTimer() {
     // save current alarm info
     const totalSeconds = await getRemainingTime();
-    
+    chrome.storage.local.set({ remainingSeconds : totalSeconds });
 
     // clear alarm
+    chrome.alarms.clear('timer');
 
+    document.getElementById("start-btn").innerText = "Start";
+    updateTimeDisplay();
+}
+
+function resetTimer() {
+    chrome.action.setBadgeText({ text: '' });
+    chrome.alarms.clearAll();
+
+    document.getElementById("start-btn").innerText = "Pause";
+    chrome.storage.local.remove('remainingSeconds');
+    updateTimeDisplay();
 }
 
 async function updateTimeDisplay() {
-    const totalSeconds = await getRemainingTime();
     const displayText = document.getElementById("time");
+    const totalSeconds = await getRemainingTime();
 
     const remainingMinutes = Math.floor(totalSeconds / 60);
     const remainingSeconds = totalSeconds % 60;
@@ -33,23 +62,27 @@ async function updateTimeDisplay() {
     displayText.innerText = remainingMinutes + ":" + remainingSeconds;
 }
 
-function getRemainingTime() {
+async function getRemainingTime() {
+    const result = await chrome.storage.local.get(['remainingSeconds']);
+
     return new Promise((resolve) => {
         chrome.alarms.get('timer', function(alarm) { 
-            if (alarm) {
+            if (alarm) {  // current alarm time
                 const now = Date.now();
                 const remainingTimeMilliseconds = alarm.scheduledTime - now;
                 const totalSeconds = Math.max(0, Math.floor(remainingTimeMilliseconds / 1000));
 
                 resolve(totalSeconds);
-            } else { 
+            } else if (result.remainingSeconds != undefined) {
+                resolve(result.remainingSeconds);
+            } else {  // paused/default time
                 resolve(timesArray[currentIndex] * 60);
             }
         });
     });
 }
 
-document.getElementById("start-btn").addEventListener('click', startTimer);
+document.getElementById("start-btn").addEventListener('click', startPauseTimer);
 document.getElementById("reset-btn").addEventListener('click', resetTimer);
 
 window.onload = updateTimeDisplay();
