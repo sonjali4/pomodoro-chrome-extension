17ff67479680b80fcfc91ff1ec00f8220bf1bcbe
sonjali4 <sonjali2006@gmail.com>
2025-10-02 18:22:14 +1300

    added pixel art styling, connected number pngs to timer display

--- popup/popup.js
+++ popup/popup.js
@@ -7,13 +7,33 @@ const resetBtn = document.getElementById('reset-btn');
 
 const timeDisplay = document.getElementById('time-display');
 
+const timeMinute1 = document.getElementById('timer-number-minute-1')
+const timeMinute2 = document.getElementById('timer-number-minute-2')
+const timeSecond1 = document.getElementById('timer-number-second-1')
+const timeSecond2 = document.getElementById('timer-number-second-2')
+
 // functions
 function setTimeDisplay(secondsRemaining) {
     let minutes = Math.floor(secondsRemaining / 60);
     let seconds = secondsRemaining % 60;
 
-    let timeString = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-    timeDisplay.innerText = timeString;
+    // let timeString = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    // timeDisplay.innerText = timeString;
+
+    let minuteString = `${minutes.toString().padStart(2, "0")}`;
+    let minuteImg1 = '../images/images-numbers/time-' + minuteString.charAt(0) + '.png';
+    let minuteImg2 = '../images/images-numbers/time-' + minuteString.charAt(1) + '.png';
+
+    let secondString = `${seconds.toString().padStart(2, "0")}`;
+    let secondImg1 = '../images/images-numbers/time-' + secondString.charAt(0) + '.png';
+    let secondImg2 = '../images/images-numbers/time-' + secondString.charAt(1) + '.png';
+
+    timeMinute1.src = minuteImg1;
+    timeMinute2.src = minuteImg2;
+    timeSecond1.src = secondImg1;
+    timeSecond2.src = secondImg2;
+
+    console.log('updated url');
 }
 
 function changeDisplay(index) {
